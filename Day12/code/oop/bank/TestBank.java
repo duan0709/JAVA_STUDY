528e0e051d21f1bc930796318caf5b8b8ecdd079
duan0709 <52620141+duan0709@users.noreply.github.com>
2021-07-26 21:34:15 +0800

    Delete Day12 directory

--- Day12/code/oop/bank/TestBank.java
+++
@@ -1,8 +0,0 @@
-package com.duan.day08.oop.bank;
-
-public class TestBank {
-	public static void main(String[] args) {
-		Bank bank = new Bank();
-		bank.login();
-	}
-}
--- Day12/code/oop/demo/Dog.java
+++
@@ -1,9 +0,0 @@
-package com.duan.day08.oop.demo;
-/**
- * 
- * @author duan
- * @version 1.0
- */
-public class Dog {
-
-}
